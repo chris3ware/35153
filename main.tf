87e2ef89b5765daddc5fdff76dc43d72a5ae92da
chris3ware <36608309+chris3ware@users.noreply.github.com>
2025-04-03 10:37:10 +0100

    add provider configuration

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = "~> 1.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.90"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.6"
+    }
+    sops = {
+      source  = "carlpett/sops"
+      version = "~> 1.1"
+    }
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "~> 0.64"
+    }
+  }
+}
